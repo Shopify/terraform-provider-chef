c7716cbd5f3f4bb8c46ff46d1600dc5c1ece1be2
Ryan Yabuta <yabuta@triggit.com>
2015-04-08 01:40:43 -0700

    Put binary in same repo

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "github.com/rsyabuta/terraform-chef-provider/chef"
+  "github.com/hashicorp/terraform/plugin"
+)
+
+func main() {
+  plugin.Serve(&plugin.ServeOpts{
+    ProviderFunc: chef.Provider,
+  })
+}
